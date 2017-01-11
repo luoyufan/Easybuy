2c269d9bf712d7e109aa5334e941f13a9beca218
Administrator <765837479@qq.com>
2017-01-11 17:38:13 +0800

    entity

--- src/com/snail/easybuy/entity/Comment.java
+++ src/com/snail/easybuy/entity/Comment.java
@@ -1,8 +1,50 @@
 package com.snail.easybuy.entity;
 
+import java.util.Date;
+
 public class Comment {
-	public static void main(String[] args) {
-		
+	private  int  EC_ID ;    
+	private  String  EC_REPLY ;    
+	private  String  EC_CONTENT ;    
+	private  Date  EC_CREATE_TIME ;    
+	private  Date  EC_REPLY_TIME ;    
+	private  String  EC_NICK_NAME ;
+	public int getEC_ID() {
+		return EC_ID;
 	}
-	
+	public void setEC_ID(int eC_ID) {
+		EC_ID = eC_ID;
+	}
+	public String getEC_REPLY() {
+		return EC_REPLY;
+	}
+	public void setEC_REPLY(String eC_REPLY) {
+		EC_REPLY = eC_REPLY;
+	}
+	public String getEC_CONTENT() {
+		return EC_CONTENT;
+	}
+	public void setEC_CONTENT(String eC_CONTENT) {
+		EC_CONTENT = eC_CONTENT;
+	}
+	public Date getEC_CREATE_TIME() {
+		return EC_CREATE_TIME;
+	}
+	public void setEC_CREATE_TIME(Date eC_CREATE_TIME) {
+		EC_CREATE_TIME = eC_CREATE_TIME;
+	}
+	public Date getEC_REPLY_TIME() {
+		return EC_REPLY_TIME;
+	}
+	public void setEC_REPLY_TIME(Date eC_REPLY_TIME) {
+		EC_REPLY_TIME = eC_REPLY_TIME;
+	}
+	public String getEC_NICK_NAME() {
+		return EC_NICK_NAME;
+	}
+	public void setEC_NICK_NAME(String eC_NICK_NAME) {
+		EC_NICK_NAME = eC_NICK_NAME;
+	}    
+
+
 }
